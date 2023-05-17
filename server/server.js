febec0a18b28e7803b2645005e8b751408bb259a
Kevin Fan <kevinlifan@yahoo.com>
2023-05-17 10:06:24 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -32,6 +32,9 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}...`);
+// });
+
+
+module.exports = app.listen(port, () => console.log(`Listening on port ${port}`));
